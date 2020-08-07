5bc115139c53219b39ba613ec25b26378a59c3d6
Alexey <peneksem@gmail.com>
2020-08-07 12:41:19 +0300

    Общие стили прописаны, плавный скрол и картинки

--- gulpfile.js
+++ gulpfile.js
@@ -120,14 +120,14 @@ function jsLib() {
 // Оптимизация изображений
 function images() {
 	return src(path.src.img)
-		.pipe(webp({quality: 70}))
+		.pipe(webp({quality: 80}))
 		.pipe(dest(path.build.img))
 		.pipe(src(path.src.img))
 		.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{ removeViewBox: false }],
 			interlaced: true,
-			optimizationLevel: 3 //0 to 7
+			optimizationLevel: 2 //0 to 7
 		}))
 		.pipe(dest(path.build.img))
 		.pipe(browsersync.stream())
